031b7a6f040f8dd6b13fb1f281022e43114ea1da
antonio.miranda <complexresponse@gmail.com>
2013-03-19 16:11:54 +0100

    Add helpers for js and css files

--- controller.rb
+++ controller.rb
@@ -1,7 +1,14 @@
 ignore /layout.html.haml/
-ignore /.gitignore/
+ignore /\.gitignore/
 ignore /README/
 
+layout 'layout.html.haml'
+
 before 'index.html.haml' do
-  layout 'layout.html.haml'
+	def include_js(file)
+		"<script src='#{file}'></script>"
+	end
+	def include_css(file)
+		"<link href='#{file}' rel='stylesheet' />"
+	end
 end
