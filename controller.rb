10aa8cf4f58e80b4964edfc62feb407bcae21e70
antonio miranda <complexresponse@gmail.com>
2013-03-20 23:47:01 +0100

    Add modernizr, refactor helper methods

--- controller.rb
+++ controller.rb
@@ -5,10 +5,10 @@ ignore /README/
 layout 'layout.html.haml'
 
 before 'index.html.haml' do
-	def include_js(file)
+	def javascript_include_tag(file)
 		"<script src='#{file}'></script>"
 	end
-	def include_css(file)
+	def stylesheet_link_tag(file)
 		"<link href='#{file}' rel='stylesheet' />"
 	end
 end
