be17bfc1bb31ad72b28c349d9739fbb43a8b3f91
Antonio Miranda <antsmiranda@gmail.com>
2015-04-19 17:43:33 +0200

    Change copy task to include all static assets

--- Gulpfile.js
+++ Gulpfile.js
@@ -7,34 +7,34 @@ var paths = {
     dist: './dist/',
     app: './app/'
 };
-gulp.task('jade', function() {
-    gulp.src(paths.app + 'index.jade')
-        .pipe(jade({
-            pretty: true
-        }))
-        .pipe(gulp.dest(paths.dist));
+
+gulp.task( 'jade', function() {
+  gulp.src( paths.app + 'index.jade' )
+    .pipe( jade({
+      pretty: true
+    }))
+    .pipe( gulp.dest( paths.dist ));
 });
 
-gulp.task('copy', function() {
-  gulp.src([
-    paths.app + 'img/**/*'
-    ]).pipe(gulp.dest(paths.dist + 'img'));
+gulp.task( 'copy_assets', function() {
+  gulp.src( paths.app + 'assets/**/*' )
+    .pipe( gulp.dest( paths.dist ));
 });
 
-gulp.task('sass', function () {
-    gulp.src(paths.app + 'sass/styles.sass')
-        .pipe(sass({
-            indentedSyntax: true
-        }))
-        .pipe(gulp.dest(paths.dist + 'css'));
+gulp.task( 'sass', function () {
+  gulp.src( paths.app + 'sass/styles.sass' )
+    .pipe( sass({
+      indentedSyntax: true
+    }))
+    .pipe( gulp.dest( paths.dist + 'css' ));
 });
 
-gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: "./dist/"
-        }
-    });
+gulp.task( 'browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: "./dist/"
+    }
+  });
 });
 
-gulp.task('default', ['copy', 'sass', 'jade', 'browser-sync']);
+gulp.task( 'default', [ 'copy_assets', 'sass', 'jade', 'browser-sync' ]);
