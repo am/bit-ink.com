a639e486a959258ced4166ca453b6dad5de29190
Antonio Miranda <antsmiranda@gmail.com>
2015-07-09 00:09:16 +0200

    Send pageviews to GA on pjax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,9 +2,19 @@
     // settings
     // the timeout is high since the response could not be cached
     $.pjax.defaults.timeout = 2000;
+
+    var updatePrism = function() {
+        Prism.highlightAll();
+    };
+    var updateGA = function() {
+        Prism.highlightAll();
+    };
+
     // events bind
     $(document).on('pjax:complete', function() {
-        Prism.highlightAll();
+        updatePrism();
+        updateGA();
     });
+    // init pjax
     $(document).pjax('a[data-pjax]', '#viewsource');
 }());
