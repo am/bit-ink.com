505cab4b0e3efc67a6dcc54d2b965116efe25968
Antonio Miranda <antsmiranda@gmail.com>
2015-07-08 22:51:50 +0200

    Update Prism after pjax request

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,10 @@
 (function () {
-    console.log('app initialize.');
+    // settings
     // the timeout is high since the response could not be cached
     $.pjax.defaults.timeout = 2000;
+    // events bind
+    $(document).on('pjax:complete', function() {
+        Prism.highlightAll();
+    });
     $(document).pjax('a[data-pjax]', '#viewsource');
 }());
