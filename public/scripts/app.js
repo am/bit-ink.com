53566c31c105d0b11946e605c3902a1410abbf3c
Antonio Miranda <antsmiranda@gmail.com>
2015-08-31 23:56:00 +0200

    Animate viewsource height once content is loaded

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,11 +10,22 @@
         Prism.highlightAll();
     };
 
+    var updateUI = function () {
+        $('.viewsource').height($('#viewsource').height());
+    };
+
     // events bind
     $(document).on('pjax:complete', function() {
         updatePrism();
         updateGA();
     });
+
+    $(document).on('pjax:end', function() {
+        updateUI();
+    });
     // init pjax
     $(document).pjax('a[data-pjax]', '#viewsource');
+    // update UI at first load
+    updateUI();
 }());
+p
