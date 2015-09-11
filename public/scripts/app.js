f60c418b595fe723daae557a019b219733d6f653
Antonio Miranda <antsmiranda@gmail.com>
2015-09-11 02:13:19 +0200

    Add browserify bundle build

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,16 @@
-(function () {
+(function(){
+    'use strict';
+
+    Prism = require('../../node_modules/prismjs/components/prism-core.js');
+    require('../../node_modules/prismjs/components/prism-markup.js');
+    require('../../node_modules/prismjs/components/prism-markdown.js');
+    require('../../node_modules/prismjs/components/prism-clike.js');
+    require('../../node_modules/prismjs/components/prism-javascript.js');
+    require('../../node_modules/prismjs/components/prism-coffeescript.js');
+    require('../../node_modules/prismjs/components/prism-jade.js');
+    require('../../node_modules/prismjs/components/prism-stylus.js');
+    require('../../node_modules/prismjs/components/prism-css.js');
+
     // settings
     // the timeout is high since the response could not be cached
     $.pjax.defaults.timeout = 2000;
@@ -29,4 +41,5 @@
     $(document).pjax('a[data-pjax]', '#viewsource');
     // update UI at first load
     updateUI();
+
 }());
