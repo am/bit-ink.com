95732003909e21864f37c308f014c4897d8439a6
antonio miranda <antsmiranda@gmail.com>
2018-11-01 18:17:42 +0100

    Add vertical rhythm

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-nested'),
+    require('postcss-gridlover'),
     require('postcss-normalize'),
     require('postcss-preset-env')({stage: 0}),
     require('lost')
